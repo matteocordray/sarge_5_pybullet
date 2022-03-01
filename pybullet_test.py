f68d86e82a04a7a0e15177727a51cd5f91c0bd4f
Matteo Cordray <matteo.cordray@gmail.com>
2022-03-01 17:02:25 -0500

    updated robot urdf

--- pybullet_test.py
+++ pybullet_test.py
@@ -4,26 +4,13 @@ import time
 import pybullet_data
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
+p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-shape_id = p.createCollisionShape(
-    shapeType=p.GEOM_MESH,
-    fileName="OBJ/Sarge_5_Body_Frame_v1.obj",
-    meshScale=[0.25,0.25,0.25]
-)
-cubePosition = [0, 0, 0]
+
 cubeStartOrientation = p.getQuaternionFromEuler([1.57075,0,0])
+cubeStartPos = [0,0,1]
 
-body_id = p.createMultiBody(
-    baseCollisionShapeIndex=shape_id,
-    basePosition=(cubePosition[0], cubePosition[1], cubePosition[2]),
-    baseOrientation=(cubeStartOrientation[0], cubeStartOrientation[1], 
-                     cubeStartOrientation[2], cubeStartOrientation[3]),
-)
-
-cubeStartPos = [0,0,0.5]
-
-# boxId = p.loadURDF("URDF/04-materials.urdf",cubeStartPos, cubeStartOrientation)
+sarge5_Id = p.loadURDF("URDF/05-sarge5_v1.urdf",cubeStartPos, cubeStartOrientation)
 while(1):
     p.stepSimulation()
     time.sleep(1./240.)
